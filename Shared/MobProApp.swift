0df82f3f9c97f7b8ab3106fd123bb8c2598d4974
Tom Phillips <tomphillipsmusic@gmail.com>
2022-07-07 10:48:12 -0400

    Initial Commit

---
+++ Shared/MobProApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobProApp.swift
+//  Shared
+//
+//  Created by Tom Phillips on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MobProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
